8ba43e1dc7ac551a7594b01ef19e0bf434078444
nkorporaal <nap.korporaal+github@gmail.com>
2025-02-04 13:11:39 +0100

    chore: verwijder duplicate sortering

--- src/Rvig.Data.Bewoningen/Services/GetAndMapGbaBewoningenService.cs
+++ src/Rvig.Data.Bewoningen/Services/GetAndMapGbaBewoningenService.cs
@@ -103,10 +103,6 @@ public class GetAndMapGbaBewoningenService : GetAndMapGbaServiceBase, IGetAndMap
 			List<(bewoning_bewoner dbBewoner, long plId)> allDbBewonersPlIds = dbBewoningWrapper.Bewoners
 				.Where(bewonerPlId => bewonerPlId.adres_verblijf_plaats_ident_code?.Equals(identificatie) == true)
 				.Select(bewoner => (dbBewoner: bewoner, plId: bewoner.pl_id))
-				.OrderBy(bewoner => bewoner.dbBewoner.vb_adreshouding_start_datum)
-				.ThenBy(bewoner => bewoner.dbBewoner.geslachts_naam)
-				.ThenBy(bewoner => bewoner.dbBewoner.voor_naam)
-				.ThenBy(bewoner => bewoner.dbBewoner.geboorte_datum)
 				.ToList();
 
 			if (peildatum.HasValue && !van.HasValue && !tot.HasValue)
