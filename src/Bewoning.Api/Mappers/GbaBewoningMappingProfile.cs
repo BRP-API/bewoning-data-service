132bcf02147621454fba7a811b8dbf86ce8e8698
nkorporaal <nap.korporaal+github@gmail.com>
2025-03-07 16:59:16 +0100

    fix: map naam

--- src/Bewoning.Api/Mappers/GbaBewoningMappingProfile.cs
+++ src/Bewoning.Api/Mappers/GbaBewoningMappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Bewoning.Api.ApiModels.Bewoning;
+using Bewoning.Api.ApiModels.PersonenHistorieBase;
 
 namespace Bewoning.Api.Mappers
 {
@@ -15,6 +16,14 @@ namespace Bewoning.Api.Mappers
                 .ReverseMap();
 
             CreateMap<GbaBewoner, Generated.GbaBewoner>()
+                .ForMember(dest => dest.Naam, opt => opt.MapFrom(src => src.Naam))
+                .ReverseMap();
+
+            CreateMap<GbaNaamBasis, Generated.NaamBasis>()
+                .ForMember(dest => dest.Voornamen, opt => opt.MapFrom(src => src.Voornamen))
+                .ForMember(dest => dest.AdellijkeTitelPredicaat, opt => opt.MapFrom(src => src.AdellijkeTitelPredicaat))
+                .ForMember(dest => dest.Voorvoegsel, opt => opt.MapFrom(src => src.Voorvoegsel))
+                .ForMember(dest => dest.Geslachtsnaam, opt => opt.MapFrom(src => src.Geslachtsnaam))
                 .ReverseMap();
         }
     }
