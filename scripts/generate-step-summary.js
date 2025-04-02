a4ce49616c354f78ae5d11680f60770b8395011e
nkorporaal <nap.korporaal+github@gmail.com>
2025-04-02 11:51:51 +0200

    ci: paths bijgewerkt in fileMap

--- scripts/generate-step-summary.js
+++ scripts/generate-step-summary.js
@@ -7,12 +7,12 @@ fs.writeFileSync(outputFile, '', 'utf8');
 
 const fileMap = new Map([
     ["./../test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt", "docs (zonder integratie)"],
-    ["./../test-reports/cucumber-js/step-definitions/test-result-raadpleeg-bewoning-met-periode-summary.txt", "raadpleeg bewoning met periode"],
-    ["./../test-reports/cucumber-js/step-definitions/test-result-raadpleeg-bewoning-op-peildatum-summary.txt", "raadpleeg bewoning op peildatum"],
-    ["./../test-reports/cucumber-js/step-definitions/test-result-geboortedatum-summary.txt", "geboortedatum"],
-    ["./../test-reports/cucumber-js/step-definitions/test-result-geslacht-summary.txt", "geslacht"],
-    ["./../test-reports/cucumber-js/step-definitions/test-result-naam-summary.txt", "naam"],
-    ["./../test-reports/cucumber-js/step-definitions/test-result-protocollering-summary.txt", "protocollering"]
+    ["./../test-reports/cucumber-js/bewoning/test-result-raadpleeg-bewoning-met-periode-summary.txt", "raadpleeg bewoning met periode"],
+    ["./../test-reports/cucumber-js/bewoning/test-result-raadpleeg-bewoning-op-peildatum-summary.txt", "raadpleeg bewoning op peildatum"],
+    ["./../test-reports/cucumber-js/bewoning/test-result-geboortedatum-summary.txt", "geboortedatum"],
+    ["./../test-reports/cucumber-js/bewoning/test-result-geslacht-summary.txt", "geslacht"],
+    ["./../test-reports/cucumber-js/bewoning/test-result-naam-summary.txt", "naam"],
+    ["./../test-reports/cucumber-js/bewoning/test-result-protocollering-summary.txt", "protocollering"]
 ]);
 
 fileMap.forEach((caption, filePath) => {
