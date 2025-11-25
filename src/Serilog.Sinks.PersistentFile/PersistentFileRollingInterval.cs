d5270ce3a37bc240dc2b03c857dd0ed8e413b53d
MelvLee <github-action@actions-template-sync.noreply.github.com>
2025-11-25 00:16:11 +0000

    chore: sync met 'BRP-API/brp-shared-dotnet' repo
    Signed-off-by: MelvLee <github-action@actions-template-sync.noreply.github.com>

--- src/Serilog.Sinks.PersistentFile/PersistentFileRollingInterval.cs
+++ src/Serilog.Sinks.PersistentFile/PersistentFileRollingInterval.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace Serilog.Sinks.PersistentFile
+namespace Serilog
 {
     /// <summary>
     /// Specifies the frequency at which the log file should roll.
